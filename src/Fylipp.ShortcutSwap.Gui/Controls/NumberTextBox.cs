17100041f8c65b937e6cbb112569eddd73685e37
Andreas Hässler <andreas.haessler@outlook.at>
2017-07-09 10:51:44 +0200

    Add swapping UI

---
+++ src/Fylipp.ShortcutSwap.Gui/Controls/NumberTextBox.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace Fylipp.ShortcutSwap.Gui.Controls {
+    class NumberTextBox : TextBox {
+
+        public long Number {
+            get {
+                return long.Parse(Text);
+            }
+            set {
+                var text = value.ToString();
+
+                if (text.Length > MaxLength) {
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                }
+
+                Text = value.ToString();
+            }
+        }
+
+        public NumberTextBox(): base() {
+            PreviewTextInput += OnTextInput;
+        }
+
+        private void OnTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e) {
+            if (!char.IsDigit(e.Text, e.Text.Length - 1)) {
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ src/Fylipp.ShortcutSwap.Gui/Controls/PathPicker.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fylipp.ShortcutSwap.Gui.Controls {
+
+    /// <summary>
+    /// Interaction logic for PathPicker.xaml
+    /// </summary>
+    public partial class PathPicker : UserControl {
+
+        public bool IsFolderPicker { get; set; } = false;
+
+        public PathPicker() {
+            InitializeComponent();
+
+            button.Click += (sender, args) => {
+                BrowsePath();
+            };
+        }
+
+        public void BrowsePath() {
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = IsFolderPicker;
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok) {
+                textBox.Text = dialog.FileName;
+            }
+        }
+    }
+
+}
